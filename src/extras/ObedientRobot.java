2cb3fa537993dcb26bb47e718685ca2d318f97f9
Punky64 <league@WTS-iMac-11.attlocal.net>
2018-02-05 16:28:07 -0800

    obedient robot
    obey, my mecha-minion!

--- src/extras/ObedientRobot.java
+++ src/extras/ObedientRobot.java
@@ -1,5 +1,9 @@
 package extras;
 
+import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class ObedientRobot {
@@ -8,13 +12,43 @@ public static void main(String[] args) {
 	t.setSpeed(10);
 	t.penDown();
 	
-	
-drawSquare(t);
-	
-	drawTriangle(t);
-	
-	drawCircle(t);
+	String t2 = JOptionPane.showInputDialog("What shape do you want?");
+String t3 = JOptionPane.showInputDialog("And what color?");
+if(t3.equalsIgnoreCase("blue")) {
+    colorBlue(t);}
 
+else if(t3.equalsIgnoreCase("green")) {
+    colorGreen(t);}
+
+else if(t3.equalsIgnoreCase("purple")) {
+    colorPurple(t);}
+
+else if(t3.equalsIgnoreCase("yellow")) {
+    colorYellow(t);}
+
+else if(t3.equalsIgnoreCase("red")) {
+    colorRed(t);}
+
+else if(t3.equalsIgnoreCase("orange")) {
+    colorOrange(t);}
+
+else {
+	JOptionPane.showMessageDialog(null, "I dont know that color. Cancelling color change.");
+}
+if(t2.equalsIgnoreCase("square")) {
+    drawSquare(t);}
+	else if(t2.equalsIgnoreCase("triangle")) {
+	drawTriangle(t);}
+	else if(t2.equalsIgnoreCase("circle")) {
+	drawCircle(t);}
+	else {
+		JOptionPane.showMessageDialog(null, "I don't know that shape. Cancelling shape.");
+	}
+	
+	
+	
+	
+	
 	
 	
 	}
@@ -25,7 +59,7 @@ for(int i=3;i>0;i--) {
 
 static void drawCircle(Robot t) {
 	for(int i=200;i>0;i--) {
-		t.move(1);
+		t.move(2);
 		t.turn(2);
 	}
 }
@@ -34,4 +68,23 @@ static void drawSquare(Robot t){
 	for(int i=4;i>0;i--) {
 	t.move(100);
 	t.turn(90);}}
+static void colorBlue(Robot t){
+	t.setPenColor(Color.blue);}
+
+static void colorGreen(Robot t){
+	t.setPenColor(Color.green);}
+
+static void colorPurple(Robot t){
+	t.setPenColor(Color.magenta);}
+
+static void colorYellow(Robot t){
+	t.setPenColor(Color.yellow);}
+
+static void colorRed(Robot t){
+	t.setPenColor(Color.red);}
+
+static void colorOrange(Robot t){
+	t.setPenColor(Color.orange);}
 }
+
+
